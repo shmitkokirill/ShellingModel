2278b542f5559d8259b7f318c6958d9961ef7eff
Kirill <kirill@MacBook-kirill.local>
2023-11-28 20:31:35 +0300

    added method collectUnhappyCells

--- Shelling.py
+++ Shelling.py
@@ -1,4 +1,5 @@
 import random
+from copy import copy
 
 class Color:
     BLUE    = 1
@@ -12,6 +13,7 @@ class ColUnit:
         self.count = int(total_cells * ratio)
         self.x = 0
         self.y = 0
+        self.is_happy = False
 
 class ShellingModel:
     def __init__(self, size: int):
@@ -27,6 +29,8 @@ class ShellingModel:
         }
 
         self.__grid = []
+        self.__empty_cells = []
+        self.__unhappy_cells = []
         self.__defaultInitGrid()
 
     def print(self) -> None:
@@ -35,17 +39,45 @@ class ShellingModel:
                 print(col_unit.value, end=' ')
             print()
 
+    def printCoords(self):
+        for row in self.__grid:
+            for col_unit in row:
+                print('{' + str(col_unit.x) + ',' + str(col_unit.y) + '}', end=' ')
+            print()
+
+
+    def printUnhappy(self) -> None:
+        for col_unit in self.__unhappy_cells:
+            print('{' + str(col_unit.x) + ',' + str(col_unit.y) + '}', end=' ')
+
+    def __collectUnhappyCells(self):
+        grid = self.__grid
+        for i in range(len(grid)):
+            # for each cell
+            for j in range(len(grid[i])):
+                cell = grid[i][j]
+
+                same_color_neighbors = 0
+                # check around current cell
+                for x in range(max(0, i - 1), min(i + 2, len(grid))):
+                    for y in range(max(0, j - 1), min(j + 2, len(grid[i]))):
+                        if (x, y) != (i, j) and grid[x][y].value == cell.value:
+                            same_color_neighbors += 1
+
+                if same_color_neighbors < 2:
+                    self.__unhappy_cells.append(cell)
 
     def __fillInSingle(self, col_unit : ColUnit):
         for _ in range(col_unit.count):
+            c_cpy = copy(col_unit)
             while True:
                 x = random.randint(0, self.__n - 1)
                 y = random.randint(0, self.__n - 1)
                 g = self.__grid[x][y]
                 if g.value == self.__colors[Color.EMPTY].value:
-                    col_unit.x = g.x
-                    col_unit.y = g.y
-                    self.__grid[x][y] = col_unit
+                    c_cpy.x = g.x
+                    c_cpy.y = g.y
+                    self.__grid[x][y] = c_cpy
                     break
 
     def __defaultInitGrid(self) -> None:
@@ -53,7 +85,7 @@ class ShellingModel:
         for i in range(self.__n):
             row = []
             for j in range(self.__n):
-                col_unit = self.__colors[Color.EMPTY]
+                col_unit = copy(self.__colors[Color.EMPTY])
                 col_unit.x = i
                 col_unit.y = j
                 row.append(col_unit)
@@ -61,3 +93,5 @@ class ShellingModel:
         
         for color, col_unit in self.__colors.items():
             self.__fillInSingle(col_unit)
+        
+        self.__collectUnhappyCells()
--- main.py
+++ main.py
@@ -2,3 +2,7 @@ from Shelling import ShellingModel
 
 sm = ShellingModel(10)
 sm.print()
+print()
+sm.printCoords()
+print()
+sm.printUnhappy()
